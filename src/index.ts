9d64fc036c7ab5f8f692e2a032504b62a39b5de1
colburncodes <colburnsanders@gmail.com>
2023-06-13 06:50:54 -0500

    refactor: :recycle: update to aws sdk from v2 to v3

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import {
   dynamodbDescribeTable,
-  dynamodbScanTable,
-  dynamodbUpdateTweet,
-  getAllScannedResults,
+  // dynamodbScanTable,
+  // dynamodbUpdateTweet,
+  // getAllScannedResults,
+  // sqsSendMessage,
 } from "./aws";
 import { Vendor } from "./types/vendor";
 
@@ -11,8 +12,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const init = async () => {
-  // const res = await dynamodbDescribeTable("vendors");
-  // console.log(res);
+  const res = await dynamodbDescribeTable("vendors");
+  console.log(res);
 
   // const scan = dynamodbScanTable("vendors", 5);
   // console.log((await scan.next()).value);
@@ -22,31 +23,36 @@ const init = async () => {
   // );
   // console.log("Vendors ", vendors);
 
-  const tweet = await dynamodbUpdateTweet(
-    process.env.AWS_VENDORS_TABLE_NAME ?? "",
-    {
-      id: "tweet1",
-      userId: "DCTacoTruck",
-      name: "DC Taco Truck",
-      text: "Test tweet",
-      date: "06/02/23",
-      geo: {
-        id: "geo1",
-        name: "Geo location 1",
-        place_type: "place 1",
-        full_name: "place 1",
-        country: "USA",
-        country_code: "USA",
-        coordinates: {
-          lat: 34.01283,
-          long: 41.1818,
-        },
-      },
-    },
-    "DCTacoTruck"
-  );
+  // const tweet = await dynamodbUpdateTweet(
+  //   process.env.AWS_VENDORS_TABLE_NAME ?? "",
+  //   {
+  //     id: "tweet1",
+  //     userId: "DCTacoTruck",
+  //     name: "DC Taco Truck",
+  //     text: "Test tweet",
+  //     date: "06/02/23",
+  //     geo: {
+  //       id: "geo1",
+  //       name: "Geo location 1",
+  //       place_type: "place 1",
+  //       full_name: "place 1",
+  //       country: "USA",
+  //       country_code: "USA",
+  //       coordinates: {
+  //         lat: 34.01283,
+  //         long: 41.1818,
+  //       },
+  //     },
+  //   },
+  //   "DCTacoTruck"
+  // );
 
-  console.log(tweet);
+  // console.log(tweet);
+
+  // await sqsSendMessage(
+  //   "https://sqs.us-east-2.amazonaws.com/414357402052/testqueue",
+  //   "testmessage"
+  // );
 };
 
 init();
